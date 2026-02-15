027c74790aa1878e1a65a7255000baaf40efc261
Justice Mubayiwa <j.mubayiwa@gmail.com>
2026-02-15 00:56:32 -0700

    Finalize homepage updates and performance artifacts

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+﻿module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['"Source Sans Pro"', 'sans-serif'],
+      },
+      colors: {
+        primary: '#000000',
+        accent: '#5f5f5f',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+        '128': '32rem',
+      },
+      lineHeight: {
+        'extra-tight': '1.1',
+        'relaxed': '1.75',
+      },
+      letterSpacing: {
+        wider: '0.1em',
+        widest: '0.15em',
+      }
+    },
+  },
+  plugins: [],
+};
